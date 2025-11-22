c185f1d598776c354d1c7801d1ca28c3f77bf848
Namibo06 <namibo654@gmail.com>
2025-11-22 13:36:49 -0300

    refactor: alterando nome da variavel que estava errado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./configs/ConnectDB');
+const conectaDB = require('./configs/conectaDB');
 
 const app = express();
 app.use(cors());
